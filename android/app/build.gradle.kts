8ab04b9b22917b455b77a284270f9bc43e45354d
Jonathan Garcia <20221074@uthh.edu.mx>
2025-11-06 08:19:54 -0600

    Subiendo primera versión de mi app Flutter

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,17 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
     id("com.google.gms.google-services")
+}
+
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties()
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -21,21 +29,27 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.sututeh_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            if (keystoreProperties.isNotEmpty()) {
+                keyAlias = keystoreProperties.getProperty("keyAlias")
+                keyPassword = keystoreProperties.getProperty("keyPassword")
+                storeFile = file(keystoreProperties.getProperty("storeFile"))
+                storePassword = keystoreProperties.getProperty("storePassword")
+            }
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
@@ -43,6 +57,7 @@ android {
 flutter {
     source = "../.."
 }
+
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
 }
--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,7 @@
 class ApiConfig {
-  static const String baseUrl = "http://192.168.100.9:3001";
+  //static const String baseUrl = "http://192.168.100.9:3001";
+  static const String baseUrl = "https://sututeh-server.onrender.com";
+
   
   // Endpoints
   static const String loginMobile = "$baseUrl/api/auth/mobile/login";
